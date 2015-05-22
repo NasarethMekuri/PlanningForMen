0dbd6a6cdefc522762715e12f59c70444c6cd0ff
NasarethMekuri <tm_maniac@hotmail.com>
2015-05-23 00:51:18 +0200

    Finished Retrieve methods for customers on PersonConverter. -  These methods are untested as DBHandler methods are still dummy methods, and thus they don't yet return usable data.

--- PlanningForMen/src/planningformen/business/PersonConverter.java
+++ PlanningForMen/src/planningformen/business/PersonConverter.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package planningformen.business;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import planningformen.domain.planning.Customer;
 import planningformen.technical.IOManager;
@@ -34,18 +31,80 @@ public class PersonConverter
     public Customer retrieveCustomer(String customerID)
     {
         ResultSet rs = _ioManager.getDBHandler().retrieveCustomer(customerID);
-        //TODO: convert RS to a customer and return it.
-        return null;
+        Customer customer = null;
+        try
+        {
+            if(rs.next())
+            {
+                String custID = rs.getString(1);
+                String personID = rs.getString(2);
+                String fName = rs.getString(3);
+                String lName = rs.getString(4);
+                String address = rs.getString(5);
+                String phoneNr = rs.getString(6);
+                String postalNr = rs.getString(7);
+                String email = rs.getString(8);
+                customer = new Customer(custID, personID, fName, lName, address, phoneNr, postalNr, email);
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getLocalizedMessage());
+        }
+        finally
+        {
+            try
+            {
+                rs.close();
+            }
+            catch(SQLException e)
+            {
+                System.out.println(e.getLocalizedMessage());
+                      
+            }
+        }
+        return customer;
     }
     
     public List<Customer> retrieveCustomers()
     {
         ResultSet rs = _ioManager.getDBHandler().retrieveCustomers();
-        //TODO: Convert rs to customer list and return it.
-        return null;
+        List<Customer> customers = new ArrayList<Customer>();
+        try
+        {
+            while(rs.next())
+            {
+                String custID = rs.getString(1);
+                String personID = rs.getString(2);
+                String fName = rs.getString(3);
+                String lName = rs.getString(4);
+                String address = rs.getString(5);
+                String phoneNr = rs.getString(6);
+                String postalNr = rs.getString(7);
+                String email = rs.getString(8);
+                customers.add(new Customer(custID, personID, fName, lName, address, phoneNr, postalNr, email));
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getLocalizedMessage());
+        }
+        finally
+        {
+            try
+            {
+                rs.close();
+            }
+            catch(SQLException e)
+            {
+                System.out.println(e.getLocalizedMessage());
+                      
+            }
+        }
+        return customers;
     }
     
-    public boolean updateCustomer(Customer customer) //Possibly renamte to something like updateCustomerInDB
+    public boolean updateCustomer(Customer customer) //Possibly rename to something like updateCustomerInDB
     {
         return _ioManager.getDBHandler().updateCustomer(customer.getCustomerID(), customer.getId(), customer.getFirstName(), 
                                                         customer.getLastName(), customer.getAddress(), customer.getPhoneNumber(), 
